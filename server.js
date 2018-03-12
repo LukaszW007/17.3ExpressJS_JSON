5d28554e9010d281080cd6344ddab7f6a23356f0
LukaszWiszniewski <wisznu07@gmail.com>
2018-03-12 20:40:35 +0100

    Added 4 requests

--- server.js
+++ server.js
@@ -9,3 +9,22 @@ app.get('/', function (req, res) {
 var server = app.listen(3000, function() {
     console.log('Przykładowa aplikacja nasłuchuje na http://localhost:3000');
 });
+
+app.post('/',function(req,res){
+    console.log('POST request');
+    res.send('Hello POST');
+});
+
+app.delete('/del_usr',function (req,res) {
+    console.log('DELETE request');
+    res.send('hi DELETE');
+});
+app.get('/list_user', function (req, res) {
+    console.log('Otrzymałem żądanie GET do strony /list_user');
+    res.send('Strona z listą użytkowników!');
+});
+
+app.get('/ab*cd', function(req, res) {
+    console.log('Otrzymałem żądanie GET do strony /ab*cd');
+    res.send('Wzór pasuje');
+});
